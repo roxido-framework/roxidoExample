cc0671ed2e5084052175df34b8e83bc892df6c00
David B. Dahl <dahl@stat.byu.edu>
2024-03-15 09:54:34 -0600

    Latest framework

--- R/zero.R
+++ R/zero.R
@@ -5,15 +5,15 @@
 #' Extensions*](https://cran.r-project.org/doc/manuals/R-exts.html#Zero_002dfinding)
 #'
 #' @param f A function taking a numeric vector of length one.
-#' @param guesses A numerical vector of length two giving two guesses for the
-#'   zero of the function.  The sign of the function evaluated at these two
-#'   guesses must be opposite.
+#' @param guess1 A guess for the zero of the function.
+#' @param guess2 Another guess for the zero of the function. The sign of the function
+#'   evaluated at the two guesses must be opposite.
 #' @param tol Tolerance controlling the desired precision.
 #'
 #' @export
 #' @examples
 #' cube1 <- function(x) (x^2 + 1) * (x - 1.5)
-#' zero(cube1, c(-2, 11.5))
-zero <- function(f, guesses, tol = 1e-7) {
-  .Call(.zero, f, guesses, tol)
+#' zero(cube1, -2, 11.5)
+zero <- function(f, guess1, guess2, tol = 1e-7) {
+  .Call(.zero, f, guess1, guess2, tol)
 }
