ec3d11695e16e770ecd35ba8bf458dc7d0ed2843
David B. Dahl <dahl@stat.byu.edu>
2024-04-16 15:11:32 -0600

    Update dependencies

--- src/rust/roxido_faer/src/lib.rs
+++ src/rust/roxido_faer/src/lib.rs
@@ -5,7 +5,7 @@ pub trait RMatrix2Faer {
     fn as_faer(&self) -> Result<MatRef<'static, f64>, &'static str>;
 }
 
-impl RMatrix2Faer for RObject<RMatrix, f64> {
+impl RMatrix2Faer for &RMatrix<f64> {
     fn as_faer(&self) -> Result<MatRef<'static, f64>, &'static str> {
         Ok({
             let nrow = self.nrow();
@@ -22,16 +22,16 @@ impl RMatrix2Faer for RObject<RMatrix, f64> {
     }
 }
 
-pub trait ToR<'a, RType, RMode> {
+pub trait ToR<T: RObjectVariant> {
     #[allow(clippy::mut_from_ref)]
-    fn to_r(&self, pc: &'a Pc) -> &'a mut RObject<RType, RMode>;
+    fn to_r(self, pc: &Pc) -> &mut T;
 }
 
-impl<'a> ToR<'a, RMatrix, f64> for MatRef<'a, f64> {
-    fn to_r(&self, pc: &'a Pc) -> &'a mut RObject<RMatrix, f64> {
+impl<'a> ToR<RMatrix<f64>> for MatRef<'a, f64> {
+    fn to_r(self, pc: &Pc) -> &mut RMatrix<f64> {
         let nr = self.nrows();
         let nc = self.ncols();
-        let result = RObject::<RMatrix, f64>::new(nr, nc, pc);
+        let result = RMatrix::<f64>::new(nr, nc, pc);
         for (k, r) in result.slice_mut().iter_mut().enumerate() {
             *r = self.read(k % nr, k / nc);
         }
