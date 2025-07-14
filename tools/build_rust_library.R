784d30c5362362076d2848088b49d852b796c5b1
David B. Dahl <dahl@stat.byu.edu>
2025-07-14 12:38:34 -0600

    Truncated double geometric distribution

--- tools/build_rust_library.R
+++ tools/build_rust_library.R
@@ -115,7 +115,8 @@ liba <- list.files("target", "librust.a", full.names = TRUE, recursive = TRUE)
 liba <- liba[grepl("(^|/|\\\\)release(/|\\\\)", liba)]
 liba <- if (length(liba) > 0) {
   file_info <- file.info(liba)
-  rownames(file_info)[which.max(file_info$mtime)]
+  file_info <- rownames(file_info)[which(file_info$mtime == max(file_info$mtime))]
+  file_info[which.min(nchar(file_info))]
 } else {
   message("No matching files found.")
   stop("Exiting.")
